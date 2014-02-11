d635690538a928d48d2a9b79862d20a769575ece
moakdesigns <moakdesigns@gmail.com>
2014-02-11 17:21:36 -0500

    created users, configured db to different db

---
+++ fuel/app/config/development/migrations.php
@@ -0,0 +1,25 @@
+<?php
+return array(
+	'version' => 
+	array(
+		'app' => 
+		array(
+			'default' => 
+			array(
+				0 => '001_create_admins',
+				1 => '001_create_users',
+				2 => '002_create_posts',
+				3 => '002_create_profiles',
+				4 => '003_create_users',
+			),
+		),
+		'module' => 
+		array(
+		),
+		'package' => 
+		array(
+		),
+	),
+	'folder' => 'migrations/',
+	'table' => 'migration',
+);
