a233ecfb45cd412688cade2e5f57d68e35ce2d1a
moakdesigns <moakdesigns@gmail.com>
2014-02-26 09:47:03 -0500

    removed properties arrays from client model

--- fuel/app/classes/model/client.php
+++ fuel/app/classes/model/client.php
@@ -3,40 +3,20 @@ class Model_Client extends \Orm\Model
 {
 	protected static $_properties = array(
 		'id',
-		'company' => array(
-				'data_type' => 'varchar',
-				'label' => 'Company Name',
-				'validation' => array('required'), 
-				'form' => array('type' => 'text'),
-				'default' => 'New Client',
-		),
-		'website' => array(
-				'data_type' => 'varchar', 
-				'label' => 'Website Address',
-				'validation' => array('required'),
-				'form' => array('type' => 'text'),	
-		),
-		'created_at' => array(
-				'data_type' => 'int',
-				'label' => 'Created At',
-				'form' => array(
-					'type' => false, // this prevents this field from being rendered on a form
-				),
-		),
-		'updated_at' => array(
-				'data_type' => 'int',
-				'label' => 'Updated At',
-		),
+		'company',
+		'website',
+		'created_at',
+		'updated_at',
 	);
 
 	protected static $_observers = array(
 		'Orm\Observer_CreatedAt' => array(
 			'events' => array('before_insert'),
-			'mysql_timestamp' => true,
+			'mysql_timestamp' => false,
 		),
 		'Orm\Observer_UpdatedAt' => array(
 			'events' => array('before_save'),
-			'mysql_timestamp' => true,
+			'mysql_timestamp' => false,
 		),
 	);
 
@@ -49,7 +29,6 @@ class Model_Client extends \Orm\Model
 		return $val;
 	}
 	
-	protected static $_has_many = array('contacts');
-		
+		protected static $_has_many = array('contacts');
 
 }
