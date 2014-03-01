40f844b41e49e58d1db29567d89037f3d9169b5c
Jenna Moakley <moakdesigns@gmail.com>
2014-02-28 19:40:57 -0500

    bleh

--- fuel/app/config/development/migrations.php
+++ fuel/app/config/development/migrations.php
@@ -6,16 +6,8 @@ return array(
 		array(
 			'default' => 
 			array(
-				0 => '001_create_users',
-				1 => '002_create_posts',
-				2 => '003_create_clients',
-				3 => '004_add_profile_fields_to_clients',
-				4 => '005_add_phone_to_users',
-				5 => '006_create_contacts',
-				6 => '007_add_fields_to_contacts',
-				7 => '007_rename_field_site_to_company_in_clients',
-				8 => '008_rename_field_web_address_to_website_in_clients',
-// added in above
+				0 => '009_create_clientscontacts',
+				1 => '010_create_projects',
 			),
 		),
 		'module' => 
