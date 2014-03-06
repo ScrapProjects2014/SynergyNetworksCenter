e153ac563bff4c95be0b6c982681b29791da1a34
moakdesigns <moakdesigns@gmail.com>
2014-03-06 17:32:15 -0500

    updated the db configuration file

--- fuel/app/config/test/db.php
+++ fuel/app/config/test/db.php
@@ -8,7 +8,7 @@
 return array(
 	'default' => array(
 		'connection'  => array(
-			'dsn'        => 'mysql:host=localhost;dbname=snc_dev',
+			'dsn'        => 'mysql:host=172.16.1.119;dbname=snc_dev',
 			'username'   => 'jmoakley',
 			'password'   => 'php14',
 		),
