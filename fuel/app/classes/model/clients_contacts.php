5c58ff8132e000ad0d560c6d5d614340bf10d7ce
moakdesigns <moakdesigns@gmail.com>
2014-04-02 13:31:41 -0400

    updating

--- fuel/app/classes/model/clients_contacts.php
+++ fuel/app/classes/model/clients_contacts.php
@@ -13,11 +13,11 @@ class Model_Clients_Contacts extends \Orm\Model
 	protected static $_observers = array(
 		'Orm\Observer_CreatedAt' => array(
 			'events' => array('before_insert'),
-			'mysql_timestamp' => false,
+			'mysql_timestamp' => true,
 		),
 		'Orm\Observer_UpdatedAt' => array(
 			'events' => array('before_update'),
-			'mysql_timestamp' => false,
+			'mysql_timestamp' => true,
 		),
 	);
 	protected static $_table_name = 'clients_contacts';
