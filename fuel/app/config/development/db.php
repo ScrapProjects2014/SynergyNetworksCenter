98678ea6c74cde663846f4f2ad83fcbee8011b20
Jenna Moakley <moakdesigns@gmail.com>
2014-04-08 20:47:12 -0400

    leftover changes

--- fuel/app/config/development/db.php
+++ fuel/app/config/development/db.php
@@ -7,8 +7,8 @@ return array(
 	'default' => array(
 		'connection'  => array(
 			'dsn'        => 'mysql:host=localhost;dbname=snc_dev',
-			'username'   => 'jmoakley',
-			'password'   => 'php14',
+			'username'   => 'root',
+			'password'   => 'snc2014',
 		),
 	),
 );
